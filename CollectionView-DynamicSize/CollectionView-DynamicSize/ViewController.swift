e12f71df51ce42f8b22af9a9b9a08235942c398a
Peeyush Karnwal <peeyush.karnwal@nagarro.com>
2021-06-11 11:47:46 +0530

    Initial Commit

---
+++ CollectionView-DynamicSize/CollectionView-DynamicSize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView-DynamicSize
+//
+//  Created by Peeyush Karnwal on 11/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
